617c24129417060d1bb89afb91d06c7d846f76b6
jacky <cjn154@126.com>
2020-05-29 13:53:28 +0800

    rob

---
+++ rob.py
@@ -0,0 +1,27 @@
+'''
+@description: 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统.
+如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+@param {type} 金额数组
+@return: 最高金额
+'''
+'''
+    基本DP
+    别忘了 边界情况
+'''
+from typing import List
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) < 2:
+            return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+        return dp[-1]
+
+S = Solution()
+print(S.rob([1, 2, 3, 1]))
